d8c34a5dc77cb32200dccfc7296e5d34a1a29b9b
MrZhangsGit <492574369@qq.com>
2018-09-18 10:30:57 +0800

    filter_注释

--- my-filter/src/main/java/com/my/filter/filter/FilterHandlerInterceptor.java
+++ my-filter/src/main/java/com/my/filter/filter/FilterHandlerInterceptor.java
@@ -16,6 +16,18 @@ import java.text.SimpleDateFormat;
 public class FilterHandlerInterceptor implements HandlerInterceptor {
     private static final ThreadLocal<Long> startTimeThreadLocal = new NamedThreadLocal<Long>("ThreadLocal StartTime");
 
+    /**
+     * preHandle方法是进行处理器拦截用的，顾名思义，该方法将在Controller处理之前进行调用，SpringMVC中的Interceptor拦截器
+     * 是链式的，可以同时存在
+     * 多个Interceptor，然后SpringMVC会根据声明的前后顺序一个接一个的执行，而且所有的Interceptor中的preHandle方法都会在
+     * Controller方法调用之前调用。SpringMVC的这种Interceptor链式结构也是可以进行中断的，这种中断方式是令preHandle的返
+     * 回值为false，当preHandle的返回值为false的时候整个请求就结束了。
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @return
+     * @throws Exception
+     */
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o)
             throws Exception {
@@ -34,11 +46,35 @@ public class FilterHandlerInterceptor implements HandlerInterceptor {
         return true;
     }
 
+    /**
+     * 这个方法只会在当前这个Interceptor的preHandle方法返回值为true的时候才会执行。postHandle是进行处理器拦截用的，
+     * 它的执行时间是在处理器进行处理之后，也就是在Controller的方法调用之后执行，但是它会在DispatcherServlet进行视图的
+     * 渲染之前执行，也就是说在这个方法中你可以对ModelAndView进行操作。这个方法的链式结构跟正常访问的方向是相反的，也就
+     * 是说先声明的Interceptor拦截器该方法反而会后调用，这跟Struts2里面的拦截器的执行过程有点像，只是Struts2里面
+     * 的intercept方法中要手动的调用ActionInvocation的invoke方法，Struts2中调用ActionInvocation的invoke方法就是调用下一
+     * 个Interceptor或者是调用action，然后要在Interceptor之前调用的内容都写在调用invoke之前，要在Interceptor之后调用的
+     * 内容都写在调用invoke方法之后。
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @param modelAndView
+     * @throws Exception
+     */
     @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
 
     }
 
+    /**
+     * 该方法也是需要当前对应的Interceptor的preHandle方法的返回值为true时才会执行。该方法将在整个请求完成之后，也就
+     * 是DispatcherServlet渲染了视图执行，这个方法的主要作用是用于清理资源的，当然这个方法也只能在当前这个Interceptor
+     * 的preHandle方法的返回值为true时才会执行。
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @param e
+     * @throws Exception
+     */
     @Override
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e)
             throws Exception {
